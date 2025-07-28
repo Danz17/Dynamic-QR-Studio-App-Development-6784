76135af4340cf4ce49a2401358fa595e00024572
Alaa Qweider <aqweider@gmail.com>
2025-07-28 16:33:08 -0700

    Remove unused recommendations components

--- src/components/Recommendations/index.js
+++
@@ -1,4 +0,0 @@
-export { default as RecommendationCard } from './RecommendationCard';
-export { default as RecommendationPanel } from './RecommendationPanel';
-export { default as RecommendationWidget } from './RecommendationWidget';
-export { default as SmartSuggestions } from './SmartSuggestions';
